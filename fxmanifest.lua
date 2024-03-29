182121fa9ec55b1c3658cc564d97ceaa50478cd1
Samuel Argote <samuel.argote@gmail.com>
2024-03-29 09:54:11 -0300

    Resource Manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'ziSamyy'
+description 'QB-Core Script for enable/disable the hair'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
